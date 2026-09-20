f8f60241eadd478bc3563420078921d7b6587a5f
adi <ak4564290@gmail.com>
2026-09-20 17:20:44 +0530

    REMOVE PROPERTY NAME

--- Models/DepartmentModel.cs
+++ Models/DepartmentModel.cs
@@ -10,6 +10,6 @@ namespace ASPNETMVCCRUDADOSPDROP.Models
         public int DepartmentId { get; set; }
         public string DepartmentName { get; set; }
          
-        public string name { get; set; }
+       
     }
 }
